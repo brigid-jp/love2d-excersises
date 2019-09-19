03ab5e801e00c727bb30febb1d5da227964740fd
dev <dev@brigid.jp>
2019-09-19 17:35:54 +0900

    例を修正。

--- 01/main.lua
+++ 01/main.lua
@@ -1,8 +1,25 @@
+local pcall = pcall
+
 local love = love
 local g = love.graphics
+local window = love.window
+
+local font
+local text = [[
+メロスは激怒した。必ず、かの邪智暴虐の王を除かなければならぬと決意した。メロスには政治がわからぬ。メロスは、村の牧人である。笛を吹き、羊と遊んで暮して来た。けれども邪悪に対しては、人一倍に敏感であった。きょう未明メロスは村を出発し、野を越え山越え、十里はなれた此のシラクスの市にやって来た。
+]]
+
+function love.load()
+  font = g.newFont("NotoSerifCJKjp-Regular.otf", 24)
+  local width, height, flags = window.getMode()
+  window.setMode(width, height, {
+    resizable = true;
+  })
+end
 
 function love.draw()
-  g.print("Hello World", 0, 0)
+  local width, height, flags = window.getMode()
+  g.printf(text, font, 32, 32, width - 64)
 end
 
 function love.keypressed(key)
