7703aa5c60250b860a155ca32134c7b7e02feb04
dev <dev@brigid.jp>
2019-09-19 16:46:38 +0900

    例を追加。

---
+++ 01/main.lua
@@ -0,0 +1,18 @@
+local love = love
+local g = love.graphics
+
+function love.draw()
+  g.print("Hello World", 0, 0)
+end
+
+function love.keypressed(key)
+end
+
+function love.keyreleased(key)
+end
+
+function love.textedited(text, start, length)
+end
+
+function love.textinput(text)
+end
