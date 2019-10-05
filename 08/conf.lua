4f4ddc4924466344dcd3829e9afd8481561420c6
dev <dev@brigid.jp>
2019-10-06 01:28:52 +0900

    例を追加。

---
+++ 08/conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+  t.identity = "jp.brigid.love2d-excersises.08"
+  t.appendidentity = true
+  t.gammacorrect = true
+  t.window.resizable = true
+  t.window.highdpi = true
+end
