ebf28659a91b40070a67c93e2d0e9a3d267e9beb
dev <dev@brigid.jp>
2019-10-05 23:57:25 +0900

    例を修正。

--- 05/conf.lua
+++ 05/conf.lua
@@ -3,7 +3,8 @@ function love.conf(t)
   -- t.accelerometerjoystick = true
   t.window.resizable = true
   t.window.highdpi = true
-
+  -- t.window.fullscreen = true
+  -- t.window.vsync = -1
 
   -- if love._os == "iOS" then
   --   t.window.borderless = true
--- 05/main.lua
+++ 05/main.lua
@@ -9,29 +9,47 @@ local buffer = {}
 function love.load()
   local width, height = window.getMode()
 
+  print("load", width, height)
+
   font = g.newFont("mplus-1p-regular.ttf", 24)
-  love.keyboard.setTextInput(true, 24, 24, width - 48, 24);
+  -- love.keyboard.setTextInput(true, 24, 24, width - 48, 24);
   -- love.keyboard.setTextInput(true);
 end
 
 function love.update(dt)
+  love.timer.sleep(0.02)
+end
+
+function love.resize(w, h)
+  print("resize", w, h)
+  -- love.keyboard.setTextInput(true, 0, h - 24, w, 24);
 end
 
 function love.draw()
   local width, height = window.getMode()
 
+  -- if not love.keyboard.hasTextInput() then
+  --   -- love.keyboard.setTextInput(true)
+  --   love.keyboard.setTextInput(true, 0, height - 24, width, 24);
+  --   -- love.keyboard.setTextInput(true, 0, 0, width, 24);
+  -- end
+
+  -- print("draw", width, height)
+
   if edited then
     g.setColor(1, 0, 0)
-    g.printf(edited.text, font, 24, 24, width - 48)
+    g.printf(edited.text, font, 24, height - 96, width - 48)
   end
 
   g.setColor(1, 1, 0)
-  g.printf(table.concat(buffer), font, 24, 48, width - 48)
+  g.printf(table.concat(buffer), font, 24, height - 72, width - 48)
 
   g.setColor(1, 1, 1)
   g.printf(("%s %s"):format(
       love.keyboard.hasTextInput(),
-      love.keyboard.hasScreenKeyboard()), font, 24, 96, width - 48)
+      love.keyboard.hasScreenKeyboard()), font, 24, height - 48, width - 48)
+
+  love.timer.sleep(0.015)
 end
 
 function love.keypressed(key)
@@ -66,4 +84,13 @@ function love.textinput(text)
   print("textinput", text)
   edited = nil
   buffer[#buffer + 1] = text
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+  if love.keyboard.hasTextInput() then
+    love.keyboard.setTextInput(false)
+  else
+    local width, height = window.getMode()
+    love.keyboard.setTextInput(true, 0, height - 24, width, 24);
+  end
 end
