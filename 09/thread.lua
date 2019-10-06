f1969b7011a4198eeda7d53fc200e1b73b7d0f27
dev <dev@brigid.jp>
2019-10-06 14:32:43 +0900

    例を修正。

--- 09/thread.lua
+++ 09/thread.lua
@@ -1,16 +1,37 @@
-require "love.timer"
+local filesystem = require "love.filesystem"
+local timer = require "love.timer"
+local http = require "socket.http"
+local ltn12 = require "ltn12"
 
 local urls = {
-  "test1.txt";
-  "test2.txt";
-  "test3.txt";
+  "http://brigid.jp/love2d-excersise/NotoSerifCJKjp-Regular.otf";
+  "http://brigid.jp/love2d-excersise/mplus-1p-regular.ttf";
+  -- "http://brigid.jp/love2d-excersise/";
 }
+
+local function save(file)
+  return function (chunk, e)
+    if chunk then
+      print(#chunk)
+      file:write(chunk)
+    elseif e then
+      error(e)
+    end
+    return true
+  end
+end
 
 local ch = love.thread.getChannel "status"
 
 for i = 1, #urls do
   local url = urls[i]
-  ch:push { "start", url }
-  love.timer.sleep(0.5)
-  ch:push { "done", url }
+  local filename = url:match "[^/]+$" or "tmp.dat"
+  ch:push { "fetching", url }
+  local file = assert(filesystem.newFile(filename, "w"))
+  http.request {
+    url = url;
+    sink = save(file);
+  }
+  file:close()
+  ch:push { "fetched", url }
 end
