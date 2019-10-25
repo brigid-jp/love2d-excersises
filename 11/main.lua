97bf9441b32555205df0d24a7fee021d80ca9c8d
dev <dev@brigid.jp>
2019-10-25 13:55:38 +0900

    例を修正。

--- 11/main.lua
+++ 11/main.lua
@@ -2,6 +2,12 @@ local love = love
 local g = love.graphics
 local window = love.window
 
+local mx
+local my
+local mbutton
+local mistouch
+local mpresses
+
 function love.draw()
   local width = window.getMode()
   local text = love.system.getClipboardText()
@@ -10,6 +16,17 @@ function love.draw()
   local processor_count = love.system.getProcessorCount()
   local background_music = love.system.hasBackgroundMusic()
 
+  local R, G, B, A = g.getColor()
+
+  -- open url
+  g.setColor(1, 0, 0)
+  g.rectangle("fill", 50, 50, 100, 50)
+
+  -- vibrate
+  g.setColor(0, 0, 1)
+  g.rectangle("fill", 150, 50, 100, 50)
+
+  g.setColor(R, G, B, A)
   g.printf(([[
 clipboard = %s
 power_info
@@ -19,6 +36,12 @@ power_info
 os = %s
 processor_count = %s
 background_music = %s
+mouse
+  x = %s
+  y = %s
+  button = %s
+  istouch = %s
+  presses = %s
 ]]):format(
       text or "nil",
       state or "nil",
@@ -26,5 +49,28 @@ background_music = %s
       seconds or "nil",
       os_string,
       processor_count,
-      background_music), 50, 50, width - 100)
+      background_music,
+      mx or "nil",
+      my or "nil",
+      mbutton or "nil",
+      mistouch or "nil",
+      mpresses or "nil"), 50, 100, width - 100)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+  mx = x
+  my = y
+  mbutton = button
+  mistouch = istouch
+  mpresses = presses
+
+  if 50 <= y and y < 100 then
+    if 50 <= x and x < 150 then
+      love.system.openURL("https://brigid.jp/love2d-excersise/")
+      print "open url"
+    elseif 150 <= x and x < 250 then
+      love.system.vibrate()
+      print "vibrate"
+    end
+  end
 end
