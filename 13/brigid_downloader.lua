5addba060d3aa5b972491b9ea536a9a44af8bf07
dev <dev@brigid.jp>
2020-01-06 13:56:20 +0900

    例を修正。

--- 13/brigid_downloader.lua
+++ 13/brigid_downloader.lua
@@ -7,23 +7,41 @@ local metatable = { __index = class }
 
 local id = 0
 
-local function new()
+local function new(task)
   id = id + 1
-  local channel_name = "brigid_downloader" .. id
-  local thread = love.thread.newThread "brigid_downloader_thread.lua"
-  thread:start(info, channel_name)
-
-  return { state = "processing" }
+  love.thread.newThread "brigid_downloader_thread.lua" :start(id, task)
+  return {
+    id = id;
+  }
 end
 
-function class:update()
+function class:status()
+  local result = self.result
+  if result then
+    return result, self.message
+  else
+    return "running"
+  end
 end
 
 function class:cancel()
+  local channel = love.thread.getChannel("brigid_downloader" .. self.id .. "_cancel")
+  channel:push { method = "cancel" }
+end
+
+function class:update()
+  if not self.result then
+    local channel = love.thread.getChannel("brigid_downloader" .. self.id .. "_result")
+    local response = channel:pop()
+    if response then
+      self.result = response.result
+      self.message = response.message
+    end
+  end
 end
 
 return setmetatable(class, {
-  __call = function ()
-    return setmetatable(new(), metatable)
+  __call = function (_, task)
+    return setmetatable(new(task), metatable)
   end;
 })
