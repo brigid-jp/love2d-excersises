4d20c008cc7db53c3eb1ca21ec023fcdfd2e21fb
dev <dev@brigid.jp>
2020-01-16 12:36:12 +0900

    例を修正。

--- 16/brigid/async_service.lua
+++ 16/brigid/async_service.lua
@@ -9,8 +9,40 @@ local love = {
 
 local unpack = table.unpack or unpack
 
-local class = {}
-local metatable = { __index = class }
+local queue_t = (function ()
+  local class = {}
+  local metatable = { __index = class }
+
+  function class:empty()
+    return self.m > self.n
+  end
+
+  function class:push(item)
+    local m = self.m
+    local n = self.n + 1
+    self.n = n
+    self[n] = item
+    return self
+  end
+
+  function class:pop()
+    local m = self.m
+    local n = self.n
+    if m > n then
+      return
+    end
+    local item = self[m]
+    self.m = m + 1
+    self[m] = nil
+    return item
+  end
+
+  return setmetatable(class, {
+    __call = function ()
+      return setmetatable({ m = 1; n = 0 }, metatable)
+    end;
+  })
+end)()
 
 local function new_worker(self)
   local worker_id = self.worker_id + 1
@@ -20,48 +52,41 @@ local function new_worker(self)
   local send_channel = love.thread.newChannel()
 
   local worker = {
+    id = worker_id;
     thread = thread;
     send_channel = send_channel;
   }
-
-  self.idle_workers[worker_id] = worker
-  self.spare_threads = self.spare_threads + 1
-  self.current_threads = self.current_threads + 1
+  self.workers[worker_id] = worker
+  self.worker_queue:push(worker)
 
   thread:start(worker_id, send_channel, self.recv_channel)
 
   return worker_id, worker
 end
 
+local class = {}
+local metatable = { __index = class }
+
 local function send_tasks(self)
-  local idle_workers = self.idle_workers
-  local active_workers = self.active_workers
-  local pending_tasks = self.pending_tasks
+  local worker_queue = self.worker_queue
+  local task_queue = self.task_queue
 
   while true do
-    local m = pending_tasks.m
-    local n = pending_tasks.n
-    if m > n then
+    if task_queue:empty() then
       break
     end
 
-    local worker_id, worker = next(idle_workers)
-    if not worker_id then
+    if worker_queue:empty() then
       break
     end
 
-    local task = pending_tasks[m]
-    pending_tasks.m = m + 1
-    pending_tasks[m] = nil
+    local worker = worker_queue:pop()
+
+    local task = task_queue:pop()
 
     task.status = "running"
-    task.worker_id = worker_id
 
-    idle_workers[worker_id] = nil
-    active_workers[worker_id] = worker
-    self.spare_threads = self.spare_threads - 1
-
-    worker.send_channel:push { "task", task.task_id, unpack(task) }
+    worker.send_channel:push { "task", task.id, unpack(task) }
   end
 end
 
@@ -77,17 +102,19 @@ local function new(start_threads, max_threads, max_spare_threads)
   end
 
   local self = {
-    worker_id = 0;
-    task_id = 0;
     recv_channel = love.thread.newChannel();
-    idle_workers = {};
-    active_workers = {};
-    pending_tasks = { m = 1; n = 0 };
+    worker_id = 0;
+    workers = {};
+    worker_queue = queue_t();
+
+    task_id = 0;
     tasks = {};
+    task_queue = queue_t();
+
     max_threads = max_threads;
     max_spare_threads = max_spare_threads;
-    spare_threads = 0;
-    current_threads = 0;
+    -- spare_threads = 0;
+    -- current_threads = 0;
   }
 
   for i = 1, start_threads do
@@ -99,6 +126,9 @@ end
 
 function class:update()
   local recv_channel = self.recv_channel
+  local workers = self.workers
+  local worker_queue = self.worker_queue
+
   while true do
     local recv = recv_channel:pop()
     if not recv then
@@ -110,9 +140,10 @@ function class:update()
       local worker_id = recv[2]
       local task_id = recv[3]
       print("success", worker_id, task_id)
-      self.idle_workers[worker_id] = self.active_workers[worker_id]
-      self.active_workers[worker_id] = nil
-      self.spare_threads = self.spare_threads + 1
+      -- self.idle_workers[worker_id] = self.active_workers[worker_id]
+      -- self.active_workers[worker_id] = nil
+      -- self.spare_threads = self.spare_threads + 1
+      worker_queue:push(workers[worker_id])
       send_tasks(self)
     end
 
@@ -130,18 +161,12 @@ function class:push(...)
 
   -- create task
   local task = {
-    task_id = task_id;
+    id = task_id;
     status = "pending";
     ...
   }
   self.tasks[task_id] = task
-
-  -- push pending task
-  local pending_tasks = self.pending_tasks
-  local m = pending_tasks.m
-  local n = pending_tasks.n + 1
-  pending_tasks.n = n
-  pending_tasks[n] = task
+  self.task_queue:push(task)
 
   -- send tasks
   send_tasks(self)
